e3ef966e808568de85412f00e022a951bf293efa
micto <maksimka.vlasov.2006@gmail.com>
2025-02-22 10:53:53 +1000

    labs

---
+++ Vasyliev/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+unsigned char nat[10000000 / 8];
+
+unsigned char selbit(unsigned char b, int k) {
+	return b | (0x80) >> k;
+}
+
+unsigned char clrbit(unsigned char b, int k) {
+	return ~(0x80 >> k) | b;
+}
+
+
+int testbit(unsigned char b, int k) {
+	return 0x80 >> 1 & b ? 1 : 0;
+}
+
+int main(int argc, char* argv[]) {
+		
+	long N, Prime;
+	scanf_s("%ld", &N);
+
+	for (int i = 0; i <= N / 8; i++) nat[i] = -1;
+
+
+	
+
+	return 0;
+}
