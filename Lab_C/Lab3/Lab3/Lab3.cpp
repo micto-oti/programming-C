06fb0ee7c26d43d603a15cb08f092a5e6df8561a
micto <maksimka.vlasov.2006@gmail.com>
2025-02-28 21:57:33 +1000

    lab

---
+++ Lab_C/Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define _countof(arr) (sizeof(arr) / sizeof(arr[0]))
+
+
+/*
+Ввести с консоли строку символов произвольной длины (до 80 символов). Поменять в ней все маленькие символы ‘a’ и ‘b’ на заглавные и вывести на консоль полученную строку.
+*/
+
+int main(int argc, char argv[]) 
+{
+	char u_str[80];
+
+	printf("Input str:");
+	scanf_s("%s", u_str, (unsigned)_countof(u_str));
+
+	for (int i; i < 80; i++) {
+		if (strchr(u_str, "a") == 0) {
+			u_str[i] = "A";
+		}
+	}
+
+
+
+	return 0;
+}
