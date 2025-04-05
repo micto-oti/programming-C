1e399710a981b9bb6c377a95cb01d3b103b42c16
micto <maksimka.vlasov.2006@gmail.com>
2025-04-05 14:54:14 +1000

    bvnbvnbvn

--- Lab_C/Lab6/Lab6/Lab6.cpp
+++ Lab_C/Lab6/Lab6/Lab6.cpp
@@ -1,4 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "data.txt"
+#include <string.h>
+#include <stdlib.h>
 
+
+int main() {
+
+	FILE* file1 = fopen("data.txt", "r");
+	FILE* output1 = fopen("output.txt", "w");
+	
+	int year;
+	char buffer[256];
+
+	if (file1 == NULL || output1 == NULL) {
+		printf("Can't open: 'data.txt' or 'output1.txt'");
+		return 1;
+	}
+
+	while (fgets(buffer, sizeof(buffer), file1) != NULL) {
+		char* last_str = strrchr(buffer, '@');
+		
+		if (last_str == NULL) continue;
+
+		year = atoi(last_str - 4);
+
+		if (year > 1980) {
+			fprintf(output1, "%s", buffer);
+			printf("%s", buffer);
+		}
+
+	}
+
+
+
+	return 0;
+}
