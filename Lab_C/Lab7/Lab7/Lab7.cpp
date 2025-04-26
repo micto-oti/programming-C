70011ea34918606a71930b975f4b4cf94e5ea951
micto <maksimka.vlasov.2006@gmail.com>
2025-04-26 12:02:39 +1000

    онгнопг

--- Lab_C/Lab7/Lab7/Lab7.cpp
+++ Lab_C/Lab7/Lab7/Lab7.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib> // Для qsort
+#include <cstdlib>
 
 
 struct humen {
@@ -12,7 +12,7 @@ struct humen {
     int birthYear;
 };
 
-// Функция сравнения для qsort (должна возвращать int и принимать const void*)
+// функция сравнения
 int compareHumen(const void* a, const void* b) {
     const humen* humenA = static_cast<const humen*>(a);
     const humen* humenB = static_cast<const humen*>(b);
@@ -27,19 +27,26 @@ int main() {
     int capacity = INITIAL_SIZE;
     int count = 0;
 
-    // Динамический массив вместо vector
+    // создание динамического массива
     humen* people = new humen[capacity];
 
-    // Чтение данных из файла
+    // чтение данных из файла и создание файла вывода
     std::ifstream inFile("data.txt");
     if (!inFile) {
         std::cerr << "Не удалось открыть файл для чтения." << std::endl;
         return 1;
     }
 
+    std::ofstream outFile("output.txt");
+    if (!outFile) {
+        std::cerr << "Не удалось открыть файл для записи." << std::endl;
+        delete[] people;
+        return 1;
+    }
+
     humen temp;
     while (inFile >> temp.firstName >> temp.secondName >> temp.lastName >> temp.birthYear) {
-        // Если массив заполнен, увеличиваем его размер
+        // если массив заполнен, увеличиваем его размер
         if (count == capacity) {
             capacity *= 2;
             humen* newPeople = new humen[capacity];
@@ -53,17 +60,10 @@ int main() {
     }
     inFile.close();
 
-    // Сортировка массива с помощью qsort
+    // Сортировка массива
     qsort(people, count, sizeof(humen), compareHumen);
 
     // Запись результата в файл
-    std::ofstream outFile("output.txt");
-    if (!outFile) {
-        std::cerr << "Не удалось открыть файл для записи." << std::endl;
-        delete[] people;
-        return 1;
-    }
-
     for (int i = 0; i < count; ++i) {
         outFile << people[i].firstName << " "
             << people[i].secondName << " "
@@ -80,118 +80,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-struct humen {
-    std::string firstName;
-    std::string secondName;
-    std::string lastName;
-    int birthYear;
-};
-
-// Функция для сравнения двух структур humen по году рождения
-bool compareHumen(const humen& a, const humen& b) {
-    return a.birthYear < b.birthYear;
-}
-
-int main() {
-    std::vector<humen> people;
-
-    // Чтение данных из файла
-    std::ifstream inFile("data.txt");
-    if (!inFile) {
-        std::cerr << "Не удалось открыть файл для чтения." << std::endl;
-        return 1;
-    }
-
-    std::ofstream outFile("output.txt");
-    if (!outFile) {
-        std::cerr << "Не удалось открыть файл для записи." << std::endl;
-        return 1;
-    }
-
-    humen temp;
-    while (inFile >> temp.firstName >> temp.secondName >> temp.lastName >> temp.birthYear) {
-        people.push_back(temp);
-    }
-    inFile.close();
-
-    // Сортировка вектора по году рождения
-    sort(people.begin(), people.end(), compareHumen);
-
-    // Запись результата в файл
-    for (const auto& person : people) {
-        outFile << person.firstName << " "
-            << person.secondName << " "
-            << person.lastName << " "
-            << person.birthYear << std::endl;
-    }
-    outFile.close();
-
-    std::cout << "Данные успешно обработаны и записаны в output.txt" << std::endl;
-    std::cout << "Обработано записей: " << people.size() << std::endl;
-
-    return 0;
-}
-
-
-
-
-*/
