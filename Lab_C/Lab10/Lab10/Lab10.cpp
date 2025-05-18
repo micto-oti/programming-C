31db0a80b5097e66796d9a182a8aca72b86ddf2d
micto <maksimka.vlasov.2006@gmail.com>
2025-05-18 21:21:32 +1000

    y

--- Lab_C/Lab10/Lab10/Lab10.cpp
+++ Lab_C/Lab10/Lab10/Lab10.cpp
@@ -12,12 +12,29 @@
             mx = arr[i]; \
         } \
     } \
-    
+   
+
+#define AVR_NUMS(ar, sz, vr); \
+    int res = 0; \
+    for (int i = 0; i < sz; i++) { \
+        res += ar[i]; \
+    } \
+    vr = (double)res/sz; \
+
+#define MIN_DIG(arr, size, mx) \
+    mx = arr[0]; \
+    for (int i = 1; i < size; i++) { \
+        if (arr[i] < mx) { \
+            mx = arr[i]; \
+        } \
+    } \
+
 
 /*
 Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.  
 Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество.
 */
+
 
 int main(){
 	char st[1000];
@@ -33,33 +50,30 @@ int main(){
 
         numbers = (int*)realloc(numbers, (count + 1) * sizeof(int));
         if (numbers == NULL) {
-            printf("Ошибка выделения памяти!\n");
+            printf("Ошибка выделения памяти\n");
             return 1;
         }
+
         numbers[count] = num;
         count++;
-
         token = strtok(NULL, " ");
     }
 
-    int res = 0;
-    for (int i = 0; i < count; i++) {
-        res += numbers[i];
-    }
+    
 
-    double avr = (double) res/count;
-
-    if (count == 0) {
-        printf("Не введено ни одного числа.\n");
-        return 1;
-    }
+    double avr_num;
+    AVR_NUMS(numbers, count, avr_num);
 
 
     int max_num;
     MAX_DIG(numbers, count, max_num);
 
+    int min_num;
+    MIN_DIG(numbers, count, min_num);
+
     printf("Максимальное число: %d\n", max_num);
-    printf("Среднее арифметическое: %lf", avr);
+    printf("Среднее арифметическое: %lf\n", avr_num);
+    printf("Минимальное число: %d\n", min_num);
 
 
 	return 0;
