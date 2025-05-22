72bafc9dfb4d85b4e896b5606d25494ec0b1d1a9
micto <micto.oti@gmail.com>
2025-05-22 22:42:15 +1000

    vault backup: 2025-05-22 22:42:15
    Affected files:
    .obsidian/workspace.json
    Lab_C/Lab14/.vs/Lab14/v17/.suo
    Lab_C/Lab14/.vs/Lab14/v17/DocumentLayout.backup.json
    Lab_C/Lab14/.vs/Lab14/v17/DocumentLayout.json
    Lab_C/Lab14/Lab14/Lab14.c
    Lab_C/Lab14/Lab14/x64/Debug/Lab14.log
    Lab_C/Lab14/Lab14/x64/Debug/Lab14.obj
    Lab_C/Lab14/Lab14/x64/Debug/Lab14.tlog/CL.command.1.tlog
    Lab_C/Lab14/Lab14/x64/Debug/Lab14.tlog/CL.read.1.tlog
    Lab_C/Lab14/Lab14/x64/Debug/Lab14.tlog/CL.write.1.tlog
    Lab_C/Lab14/Lab14/x64/Debug/vc143.idb
    Lab_C/Lab14/Lab14/x64/Debug/vc143.pdb

--- Lab_C/Lab14/Lab14/Lab14.c
+++ Lab_C/Lab14/Lab14/Lab14.c
@@ -1,1 +1,37 @@
-﻿
+﻿#include <stdio.h>
+#include <setjmp.h>
+
+int fibonacci(int n) {
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void fib_sum(jmp_buf env, int* count, int limit, int* sum) {
+    if (*count >= limit) {
+        longjmp(env, 1);
+    }
+
+    (*count)++;
+    *sum += fibonacci(*count);
+
+    fib_sum(env, count, limit, sum);
+}
+
+int main() {
+    jmp_buf jump_buffer;
+    int count = 0;
+    int limit = 0;
+    int sum = 0;
+
+    printf("Введите количество чисел Фибоначчи для суммирования: ");
+    scanf("%d", &limit);
+
+    if (setjmp(jump_buffer) == 0) {
+        fib_sum(jump_buffer, &count, limit, &sum);
+    }
+    else {
+        printf("Сумма первых %d чисел Фибоначчи: %d\n", limit, sum);
+    }
+
+    return 0;
+}
