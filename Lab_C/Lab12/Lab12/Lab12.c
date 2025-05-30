2161393d892fb0d8901b3f3aeb567768f05616a0
micto <micto.oti@gmail.com>
2025-05-30 22:31:14 +1000

    vault backup: 2025-05-30 22:31:14
    Affected files:
    .obsidian/workspace.json
    Lab_C/Lab12/Lab12/Lab12.c

--- Lab_C/Lab12/Lab12/Lab12.c
+++ Lab_C/Lab12/Lab12/Lab12.c
@@ -1,10 +1,42 @@
 ﻿#include <stdio.h>
 #include <string.h>
-#include "calendar.c"
+#include <time.h>
+#include <windows.h>
+
+// Получить день недели (0 = воскресенье ... 6 = суббота)
+int get_week(int year, int month, int day) {
+    struct tm time_info = { 0 };
+    time_info.tm_year = year - 1900;
+    time_info.tm_mon = month - 1;
+    time_info.tm_mday = day;
+    mktime(&time_info);
+    return time_info.tm_wday;
+}
+
+// Получить количество дней в месяце
+int get_month(int year, int month) {
+    switch (month) {
+    case 2:
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
+    case 4: case 6: case 9: case 11:
+        return 30;
+    default:
+        return 31;
+    }
+}
+
+void get_current(int* year, int* month, int* day) {
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    *year = st.wYear;
+    *month = st.wMonth;
+    *day = st.wDay;
+}
+
 
 int main() {
     char input[100];
-    printf("Введите дату (гггг.мм.дд / гггг.мм / гггг / now): ");
+    printf("Введите дату \nформат: \nгггг.мм.дд \nгггг.мм \nгггг \nnow): ");
     scanf("%99s", input);
 
     int year, month, day;
@@ -26,8 +58,8 @@ int main() {
         printf("\n  Календарь за %04d.%02d\n", year, month);
         printf(" Пн Вт Ср Чт Пт Сб Вс\n");
 
-        int start_day = get_day_of_week(year, month, 1);
-        int days_in_month = get_days_in_month(year, month);
+        int start_day = get_week(year, month, 1);
+        int days_in_month = get_month(year, month);
 
         for (int i = 0; i < (start_day == 0 ? 6 : start_day - 1); i++) {
             printf("   ");
@@ -46,8 +78,8 @@ int main() {
             printf("\n  Календарь за %04d.%02d\n", year, m);
             printf(" Пн Вт Ср Чт Пт Сб Вс\n");
 
-            int start_day = get_day_of_week(year, m, 1);
-            int days_in_month = get_days_in_month(year, m);
+            int start_day = get_week(year, m, 1);
+            int days_in_month = get_month(year, m);
 
             for (int i = 0; i < (start_day == 0 ? 6 : start_day - 1); i++) {
                 printf("   ");
