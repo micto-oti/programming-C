41d59f60bf98f44e56fc64933b21db01202b9f39
micto <micto.oti@gmail.com>
2025-06-14 12:58:07 +1000

    vault backup: 2025-06-14 12:58:06
    Affected files:
    .obsidian/workspace.json
    Lab_C/EXS 1/EXS 1/EXS 1.c

--- Lab_C/EXS 1/EXS 1/EXS 1.c
+++ Lab_C/EXS 1/EXS 1/EXS 1.c
@@ -23,34 +23,11 @@ int** mx_cr(int m) {
     return matrix;
 }
 
-int** mx(int n, int* arr) {
-    int r = floor(sqrt(n));
-    int** matrix = (int**)malloc(r * sizeof(int*));
-
-    for (int i = 0; i < r; i++) {
-        matrix[i] = (int*)malloc(r * sizeof(int));
-    }
-
-    int k = 0;
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < r; j++) {
-            if (k < n) {
-                matrix[i][j] = arr[k++];
-            }
-            else {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-
-    return matrix;
-}
-
 int mx_sum(int** matrix, int r) {
     int sum = 0;
-    for (int m = 0; m < r; m++) {
+    for (int i = 0; i < r; i++) {
         for (int j = 0; j < r; j++) {
-            sum += matrix[m][j];
+            sum += matrix[i][j];
         }
     }
     return sum;
@@ -60,27 +37,17 @@ int main() {
     srand(time(NULL));
     clock_t start = clock();
 
-    int i = 0, j = 0, k = 0;
-    int N;
-    int* arr1;
-    int* arr2;
-    char line[100];
-
-    /* #1 */
     int day, month, year;
     FILE* date = fopen("data.txt", "r");
-
     if (date == NULL) {
         printf("Ошибка открытия data.txt\n");
         return 1;
     }
 
-    if (fgets(line, sizeof(line), date)) {
-        if (sscanf(line, "%d.%d.%d", &day, &month, &year) != 3) {
-            printf("Неверный формат даты\n");
-            fclose(date);
-            return 1;
-        }
+    if (fscanf(date, "%d.%d.%d", &day, &month, &year) != 3) {
+        printf("Неверный формат даты\n");
+        fclose(date);
+        return 1;
     }
     fclose(date);
 
@@ -90,6 +57,7 @@ int main() {
     target.tm_mday = day;
     target.tm_mon = month - 1;
     target.tm_year = year - 1900;
+    target.tm_isdst = -1;
 
     time_t tar_time = mktime(&target);
     if (tar_time == -1) {
@@ -110,38 +78,38 @@ int main() {
         printf("Дата (%02d.%02d.%04d) прошла\n", day, month, year);
     }
 
-    printf("Введите N: ");
-    scanf("%d", &N);
-
-    int r = floor(sqrt(N));
-    arr1 = (int*)malloc(N * sizeof(int));
-
-    for (i = 0; i < N; i++) {
-        arr1[i] = rand() % 10;
-    }
+    int m;
+    printf("Введите размер матрицы m: ");
+    scanf("%d", &m);
 
     int** matrix1 = NULL;
     int** matrix2 = NULL;
-    int first = mx_sum(matrix1, r);
-    int second = -1;
+    int first_sum = -1;
+    int second_sum = -1;
     bool found = false;
 
     while (!found) {
-        int** matrix = mx_cr(r);
-        int sum = mx_sum(matrix, r);
+        int** matrix = mx_cr(m);
+        int sum = mx_sum(matrix, m);
 
-        if (matrix2 == NULL) {
+        if (matrix1 == NULL) {
+            matrix1 = matrix;
+            first_sum = sum;
+        }
+        else if (matrix2 == NULL) {
             matrix2 = matrix;
-            second = sum;
-            if (first == second) {
+            second_sum = sum;
+            if (first_sum == second_sum) {
                 found = true;
             }
         }
         else {
-            free_mx(matrix2, r);
+            free_mx(matrix1, m);
+            matrix1 = matrix2;
+            first_sum = second_sum;
             matrix2 = matrix;
-            second = sum;
-            if (first == second) {
+            second_sum = sum;
+            if (first_sum == second_sum) {
                 found = true;
             }
         }
@@ -150,37 +118,33 @@ int main() {
     FILE* matr = fopen("output.txt", "w");
     if (matr == NULL) {
         printf("Не удалось открыть файл output.txt\n");
-        free_mx(matrix1, r);
-        free_mx(matrix2, r);
-        free(arr1);
+        free_mx(matrix1, m);
+        free_mx(matrix2, m);
         return 1;
     }
 
-    fprintf(matr, "Первая матрица (сумма: %d):\n", first);
-    for (i = 0; i < r; i++) {
-        for (j = 0; j < r; j++) {
+    fprintf(matr, "Первая матрица (сумма: %d):\n", first_sum);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
             fprintf(matr, "%d ", matrix1[i][j]);
         }
         fprintf(matr, "\n");
     }
 
-    fprintf(matr, "Вторая матрица (сумма: %d):\n", second);
-    for (i = 0; i < r; i++) {
-        for (j = 0; j < r; j++) {
+    fprintf(matr, "Вторая матрица (сумма: %d):\n", second_sum);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
             fprintf(matr, "%d ", matrix2[i][j]);
         }
         fprintf(matr, "\n");
     }
 
     fclose(matr);
-    printf("Матрицы с одинаковой суммой (%d) записаны в output.txt\n", first);
-
-    free_mx(matrix1, r);
-    free_mx(matrix2, r);
-    free(arr1);
+    free_mx(matrix1, m);
+    free_mx(matrix2, m);
 
     clock_t end = clock();
     double time = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Время выполнения: %f", time);
+    printf("Время выполнения: %f\n", time);
     return 0;
 }
